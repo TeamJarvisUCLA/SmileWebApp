16c980fea2fb6a32c48e867c577aaacbc6229645
jorgeavenda <jorgeavenda.11@gmail.com>
2016-07-21 21:33:32 -0400

    para el nuevo diseño y parte de la navegabilidad

--- src/ve/smile/web/viewmodels/VM_Main.java
+++ src/ve/smile/web/viewmodels/VM_Main.java
@@ -1,5 +1,40 @@
 package ve.smile.web.viewmodels;
 
+import karen.core.crux.session.DataCenter;
+
+import org.zkoss.bind.annotation.Command;
+
 public class VM_Main {
+	
+	@Command
+	public void noticias() {
+		DataCenter.updateSrcPageContent(null, null, "/views/web/noticias.zul");
+		
+	}
+	
+	@Command
+	public void conocenos() {
+		DataCenter.updateSrcPageContent(null, null, "/views/web/conocenos.zul");
+		
+	}
+	
+	@Command
+	public void eventos() {
+		DataCenter.updateSrcPageContent(null, null, "/views/web/eventos.zul");
+		
+	}
+	
+	@Command
+	public void participa() {
+		DataCenter.updateSrcPageContent(null, null, "/views/web/participa.zul");
+		
+	}
+	
+	@Command
+	public void galeria() {
+		DataCenter.updateSrcPageContent(null, null, "/views/web/galeria.zul");
+		
+	}
+
 
 }

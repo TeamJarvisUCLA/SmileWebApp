95cda32cb7d594e4ccbee1fb44132034e3a5aa08
dlcv <dlcv@live.com.ve>
2016-07-26 18:18:15 -0400

    Añadidos: requisitos, trabajo social, clasificador de trabajo social y clasificador de sugerencias

---
+++ src/ve/smile/viewmodels/main/VMPClasificadorSugerencia.java
@@ -0,0 +1,53 @@
+package ve.smile.viewmodels.main;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import karen.core.crux.alert.Alert;
+import karen.core.simple_list_principal.viewmodels.VM_WindowSimpleListPrincipal;
+import karen.core.util.payload.UtilPayload;
+import lights.core.payload.response.IPayloadResponse;
+import ve.smile.consume.services.S ;
+import ve.smile.seguridad.enums.OperacionEnum;
+import ve.smile.dto.ClasificadorSugerencia;
+import ve.smile.payload.response.PayloadClasificadorSugerenciaResponse;
+
+import org.zkoss.bind.annotation.Init;
+
+public class VMPClasificadorSugerencia extends VM_WindowSimpleListPrincipal<ClasificadorSugerencia> {
+
+	@Init(superclass = true)
+	public void childInit() {
+		//NOTHING OK!
+	}
+
+	@Override
+	public IPayloadResponse<ClasificadorSugerencia> getDataToTable(
+			Integer cantidadRegistrosPagina, Integer pagina) {
+
+		PayloadClasificadorSugerenciaResponse payloadClasificadorSugerenciaResponse = 
+				S.ClasificadorSugerenciaService.consultarPaginacion(cantidadRegistrosPagina, pagina);
+
+		return payloadClasificadorSugerenciaResponse;
+	}
+
+	@Override
+	public void doDelete() {
+		PayloadClasificadorSugerenciaResponse payloadClasificadorSugerenciaResponse =
+				S.ClasificadorSugerenciaService.eliminar(getSelectedObject().getIdClasificadorSugerencia());
+
+		Alert.showMessage(payloadClasificadorSugerenciaResponse);
+
+		if (UtilPayload.isOK(payloadClasificadorSugerenciaResponse)) {
+			getControllerWindowSimpleListPrincipal().updateListBoxAndFooter();
+		}
+
+	}
+
+
+	@Override
+	public String getSrcFileZulForm(OperacionEnum operacionEnum) {
+		return "views/desktop/maestro/view/ClasificadorSugerenciaFormBasic.zul";
+	}
+
+}
---
+++ src/ve/smile/viewmodels/main/VMPTrabajoSocial.java
@@ -0,0 +1,53 @@
+package ve.smile.viewmodels.main;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import karen.core.crux.alert.Alert;
+import karen.core.simple_list_principal.viewmodels.VM_WindowSimpleListPrincipal;
+import karen.core.util.payload.UtilPayload;
+import lights.core.payload.response.IPayloadResponse;
+import ve.smile.consume.services.S ;
+import ve.smile.seguridad.enums.OperacionEnum;
+import ve.smile.dto.TrabajoSocial;
+import ve.smile.payload.response.PayloadTrabajoSocialResponse;
+
+import org.zkoss.bind.annotation.Init;
+
+public class VMPTrabajoSocial extends VM_WindowSimpleListPrincipal<TrabajoSocial> {
+
+	@Init(superclass = true)
+	public void childInit() {
+		//NOTHING OK!
+	}
+
+	@Override
+	public IPayloadResponse<TrabajoSocial> getDataToTable(
+			Integer cantidadRegistrosPagina, Integer pagina) {
+
+		PayloadTrabajoSocialResponse payloadTrabajoSocialResponse = 
+				S.TrabajoSocialService.consultarPaginacion(cantidadRegistrosPagina, pagina);
+
+		return payloadTrabajoSocialResponse;
+	}
+
+	@Override
+	public void doDelete() {
+		PayloadTrabajoSocialResponse payloadTrabajoSocialResponse =
+				S.TrabajoSocialService.eliminar(getSelectedObject().getIdTrabajoSocial());
+
+		Alert.showMessage(payloadTrabajoSocialResponse);
+
+		if (UtilPayload.isOK(payloadTrabajoSocialResponse)) {
+			getControllerWindowSimpleListPrincipal().updateListBoxAndFooter();
+		}
+
+	}
+
+
+	@Override
+	public String getSrcFileZulForm(OperacionEnum operacionEnum) {
+		return "views/desktop/maestro/view/TrabajoSocialFormBasic.zul";
+	}
+
+}

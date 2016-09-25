f591fd758c722324bf338f9da2ee53652f74add8
RosireMaiz <rosire08@gmail.com>
2016-09-25 00:45:59 -0400

    Modificaciones para el configurador

--- src/ve/smile/datos/configuracion/plantilla_evento/viewmodels/VM_PlantillaEventoFormBasic.java
+++ src/ve/smile/datos/configuracion/plantilla_evento/viewmodels/VM_PlantillaEventoFormBasic.java
@@ -1,13 +1,9 @@
 package ve.smile.datos.configuracion.plantilla_evento.viewmodels;
 
-import java.util.Set;
-import java.util.List;
-import java.util.HashSet;
 import java.util.ArrayList;
-
-import org.zkoss.bind.annotation.Init;
-import org.zkoss.bind.annotation.Command;
-import org.zkoss.bind.annotation.NotifyChange;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 import karen.core.crux.alert.Alert;
 import karen.core.crux.session.DataCenter;
@@ -16,296 +12,299 @@ import karen.core.form.buttons.enums.OperacionFormEnum;
 import karen.core.form.buttons.helpers.OperacionFormHelper;
 import karen.core.form.viewmodels.VM_WindowForm;
 import karen.core.util.payload.UtilPayload;
-import karen.core.util.validate.UtilValidate;
+
+import org.zkoss.bind.annotation.Command;
+import org.zkoss.bind.annotation.Init;
+import org.zkoss.bind.annotation.NotifyChange;
+
 import ve.smile.consume.services.S;
+import ve.smile.dto.ClasificadorEvento;
+import ve.smile.dto.Evento;
+import ve.smile.dto.Indicador;
+import ve.smile.dto.Tarea;
+import ve.smile.payload.response.PayloadClasificadorEventoResponse;
+import ve.smile.payload.response.PayloadEventoResponse;
+import ve.smile.payload.response.PayloadIndicadorResponse;
+import ve.smile.payload.response.PayloadTareaResponse;
 import ve.smile.seguridad.enums.OperacionEnum;
 
-import ve.smile.dto.Evento;
-import ve.smile.payload.response.PayloadEventoResponse;
-
-import ve.smile.dto.Indicador;
-import ve.smile.payload.response.PayloadIndicadorResponse;
-
-import ve.smile.dto.Tarea;
-import ve.smile.payload.response.PayloadTareaResponse;
-
-import ve.smile.payload.response.PayloadPlantillaEventoTareaResponse;
-import ve.smile.payload.response.PayloadPlantillaEventoIndicadorResponse;
-
 public class VM_PlantillaEventoFormBasic extends VM_WindowForm {
-	
-	private List <Indicador> indicadores;
-	private Set  <Indicador> indicadoresSel;
-	private List <Indicador> eventoIndicadores;
-	private Set  <Indicador> eventoIndicadoresSel;
-	
-	private List <Tarea> tareas;
-	private Set  <Tarea> tareasSel;
-	private List <Tarea> eventoTareas;
-	private Set  <Tarea> eventoTareasSel;
-	
-	public VM_PlantillaEventoFormBasic () {
-		indicadores = new ArrayList <Indicador>();
-		indicadoresSel = new HashSet <Indicador> ();
-		eventoIndicadores = new ArrayList <Indicador> ();
-		eventoIndicadoresSel = new HashSet <Indicador>();
-	}
+
+	private List<Indicador> indicadores;
+	private Set<Indicador> indicadoresSeleccionados;
+	private List<Indicador> eventoIndicadores;
+	private Set<Indicador> eventoIndicadoresSeleccionados;
+
+	private List<Tarea> tareas;
+	private Set<Tarea> tareasSeleccionadas;
+	private List<Tarea> eventoTareas;
+	private Set<Tarea> eventoTareasSeleccionadas;
+
+	private List<ClasificadorEvento> clasificadorEventos;
 	
 	@Init(superclass = true)
-	public void childInit()
-	{
-		
-	}
+	public void childInit() {
+		this.getEventoIndicadores().addAll(
+				this.getEvento().getEventoIndicadores());
 
-	// PROPIEDADES DE LAS LISTAS
-	// Indicadores
-	public List<Indicador> getIndicadores()
-	{
-		if (this.indicadores == null)
-		{
-			this.indicadores = new ArrayList<>();
-		}	
-		if (this.indicadores.isEmpty())
-		{
-			PayloadIndicadorResponse payloadIndicadorResponse = S.IndicadorService.consultarTodos();
-			if (!UtilPayload.isOK(payloadIndicadorResponse))
-			{
+		this.getEventoTareas().addAll(this.getEvento().getEventoTareas());
+
+		if (this.getIndicadores().isEmpty()) {
+			PayloadIndicadorResponse payloadIndicadorResponse = S.IndicadorService
+					.consultarTodos();
+			if (!UtilPayload.isOK(payloadIndicadorResponse)) {
 				Alert.showMessage(payloadIndicadorResponse);
+			} else {
+				indicadores.addAll(payloadIndicadorResponse.getObjetos());
 			}
-			this.indicadores.addAll(payloadIndicadorResponse.getObjetos());
 		}
-		return indicadores;
-	}
 
-	public void setIndicadores(List<Indicador> indicadores)
-	{
-		this.indicadores = indicadores;
-	}
-	
-	public Set<Indicador> getIndicadoresSel()
-	{
-		return indicadoresSel;
-	}
-
-	public void setIndicadoresSel (Set<Indicador> indicadoresSel)
-	{
-		this.indicadoresSel = indicadoresSel;
-	}
-
-	public List <Indicador> getListaIndicadores()
-	{
-		return indicadores;
-	}
-	
-	// Indicadores del evento
-	public Set<Indicador> getEventoIndicadoresSel()
-	{
-		return  eventoIndicadoresSel;
-	}
-
-	public void setEventoIndicadoresSel (Set<Indicador> eventoIndicadoresSel)
-	{
-		this.eventoIndicadoresSel = eventoIndicadoresSel;
-	}
-
-	public List <Indicador> getListaEventoIndicadores()
-	{
-		if (this.eventoIndicadores == null)
-		{
-			this.eventoIndicadores = new ArrayList<>();
-		}
-		if (this.eventoIndicadores.isEmpty())
-		{
-			PayloadPlantillaEventoIndicadorResponse payloadPlantillaEventoIndicadorResponse = S.PlantillaEventoIndicadorService.consultarPorEvento(getEvento().getIdEvento());	
-			if (!UtilPayload.isOK(payloadPlantillaEventoIndicadorResponse)) {
-				Alert.showMessage(payloadPlantillaEventoIndicadorResponse);
-			}
-			Evento e = new Evento ();				
-			e.setEventoIndicadors(payloadPlantillaEventoIndicadorResponse.getObjetos());
-			//this.eventoIndicadores = e.getEventoIndicadors(); Listas de tipo diferentes 
-		}
-		return eventoIndicadores;
-	}
-	
-	// Tareas
-	
-	public List<Tarea> getTareas()
-	{
-		if (this.tareas == null)
-		{
-			this.tareas = new ArrayList<>();
-		}
-		
-		if (this.tareas.isEmpty())
-		{
-			PayloadTareaResponse payloadTareaResponse = S.TareaService.consultarTodos();
-			if (!UtilPayload.isOK(payloadTareaResponse))
-			{
+		if (this.getTareas().isEmpty()) {
+			PayloadTareaResponse payloadTareaResponse = S.TareaService
+					.consultarTodos();
+			if (!UtilPayload.isOK(payloadTareaResponse)) {
 				Alert.showMessage(payloadTareaResponse);
+			} else {
+				tareas.addAll(payloadTareaResponse.getObjetos());
 			}
-			this.tareas.addAll(payloadTareaResponse.getObjetos());
-		}
-		return tareas;
-	}
-
-	public void setTareas(List<Tarea> tareas)
-	{
-		this.tareas = tareas;
-	}
-	
-	public Set<Tarea> getTareasSel()
-	{
-		return tareasSel;
-	}
-
-	public void setTareasSel (Set<Tarea> tareasSel) {
-		this.tareasSel = tareasSel;
-	}
-
-	public List <Tarea> getListaTareas() {
-		return tareas;
-	}
-	
-	// Tareas del evento
-	public Set<Tarea> getEventoTareasSel()
-	{
-		return  eventoTareasSel;
-	}
-
-	public void setEventoTareasSel (Set<Tarea> eventoTareasSel)
-	{
-		this.eventoTareasSel = eventoTareasSel;
-	}
-
-	public List <Tarea> getListaEventoTareas()
-	{
-		return eventoTareas;
-	}
-	
-	@Command
-	@NotifyChange({"indicadores","eventoIndicadores","indicadoresSel","eventoIndicadoresSel"})
-	public void indicadoresAPlantilla(){
-				if(indicadoresSel != null && indicadoresSel.size() > 0){
-					eventoIndicadores.addAll(indicadoresSel);
-					indicadores.removeAll(indicadoresSel);
-					eventoIndicadoresSel.addAll(indicadoresSel);
-					indicadoresSel.clear();
-				}
-			}
-	
-	@Command
-	@NotifyChange({"indicadores","eventoIndicadores","indicadoresSel","eventoIndicadoresSel"})
-	public void indicadoresFueraPlantilla(){
-		if(eventoIndicadoresSel != null && eventoIndicadoresSel.size() > 0){
-			indicadores.addAll(eventoIndicadoresSel);
-			eventoIndicadores.removeAll(eventoIndicadoresSel);
-			indicadoresSel.addAll(eventoIndicadoresSel);
-			eventoIndicadoresSel.clear();
 		}
 	}
-	
-	@Command
-	@NotifyChange({"tareas","eventoTareas","tareasSel","eventoTareasSel"})
-	public void tareasAPlantilla(){
-				if(tareasSel != null && tareasSel.size() > 0){
-					eventoTareas.addAll(tareasSel);
-					tareas.removeAll(tareasSel);
-					eventoTareasSel.addAll(tareasSel);
-					tareasSel.clear();
-				}
-			}
-	
-	@Command
-	@NotifyChange({"tareas","eventoTareas","tareasSel","eventoTareasSel"})
-	public void tareasFueraPlantilla(){
-		if(eventoTareasSel != null && eventoTareasSel.size() > 0){
-			tareas.addAll(eventoTareasSel);
-			eventoTareas.removeAll(eventoTareasSel);
-			tareasSel.addAll(eventoTareasSel);
-			eventoTareasSel.clear();
+
+	@Command("agregarIndicadoresPlantilla")
+	@NotifyChange({ "indicadores", "eventoIndicadores",
+			"indicadoresSeleccionados", "eventoIndicadoresSeleccionados" })
+	public void agregarIndicadoresPlantilla() {
+		if (this.getIndicadoresSeleccionados() != null
+				&& this.getIndicadoresSeleccionados().size() > 0) {
+			this.getEventoIndicadores().addAll(indicadoresSeleccionados);
+			this.getIndicadoresSeleccionados().clear();
+			this.getEventoIndicadoresSeleccionados().clear();
 		}
 	}
-	
+
+	@Command
+	@NotifyChange({ "indicadores", "eventoIndicadores",
+			"indicadoresSeleccionados", "eventoIndicadoresSeleccionados" })
+	public void removerIndicadoresPlantilla() {
+		if (this.getEventoIndicadoresSeleccionados() != null
+				&& this.getEventoIndicadoresSeleccionados().size() > 0) {
+			this.getEventoIndicadores().removeAll(
+					eventoIndicadoresSeleccionados);
+			this.getIndicadoresSeleccionados().clear();
+			this.getEventoIndicadoresSeleccionados().clear();
+		}
+	}
+
+	@Command
+	@NotifyChange({ "tareas", "eventoTareas", "tareasSeleccionadas",
+			"eventoTareasSeleccionadas" })
+	public void agregarTareasPlantilla() {
+		if (this.getTareasSeleccionadas() != null
+				&& this.getTareasSeleccionadas().size() > 0) {
+			this.getEventoTareas().addAll(tareasSeleccionadas);
+			this.getEventoTareasSeleccionadas().clear();
+			this.getTareasSeleccionadas().clear();
+		}
+	}
+
+	@Command
+	@NotifyChange({ "tareas", "eventoTareas", "tareasSeleccionadas",
+			"eventoTareasSeleccionadas" })
+	public void removerTareasPlantilla() {
+		if (this.getEventoTareasSeleccionadas() != null
+				&& this.getEventoTareasSeleccionadas().size() > 0) {
+			this.getEventoTareas().removeAll(eventoTareasSeleccionadas);
+			this.getTareasSeleccionadas().clear();
+			this.getEventoTareasSeleccionadas().clear();
+		}
+	}
+
+	public boolean disabledIndicador(Indicador indicador) {
+		return this.getEventoIndicadores().contains(indicador);
+	}
+
+	public boolean disabledTarea(Tarea tarea) {
+		return this.getEventoTareas().contains(tarea);
+	}
+
 	// Propiedades del formulario
 	@Override
-	public List<OperacionForm> getOperationsForm(OperacionEnum operacionEnum)
-	{
+	public List<OperacionForm> getOperationsForm(OperacionEnum operacionEnum) {
 		List<OperacionForm> operacionesForm = new ArrayList<OperacionForm>();
-		if (operacionEnum.equals(OperacionEnum.INCLUIR) || operacionEnum.equals(OperacionEnum.MODIFICAR))
-		{
-			operacionesForm.add(OperacionFormHelper.getPorType(OperacionFormEnum.GUARDAR));
-			operacionesForm.add(OperacionFormHelper.getPorType(OperacionFormEnum.CANCELAR));
+		if (operacionEnum.equals(OperacionEnum.INCLUIR)
+				|| operacionEnum.equals(OperacionEnum.MODIFICAR)) {
+			operacionesForm.add(OperacionFormHelper
+					.getPorType(OperacionFormEnum.GUARDAR));
+			operacionesForm.add(OperacionFormHelper
+					.getPorType(OperacionFormEnum.CANCELAR));
 			return operacionesForm;
 		}
 		return operacionesForm;
 	}
 
 	@Override
-	public boolean actionGuardar(OperacionEnum operacionEnum)
-	{
-		if(!isFormValidated())
-		{
+	public boolean actionGuardar(OperacionEnum operacionEnum) {
+		if (!isFormValidated()) {
 			return true;
 		}
 
-		if (operacionEnum.equals(OperacionEnum.INCLUIR))
-		{
-			/*
-			PayloadPlantillaEventoResponse payloadPlantillaEventoResponse = S.PlantillaEventoService.incluir(getEvento());
-			if(!UtilPayload.isOK(payloadPlantillaEventoResponse))
-			{
-				Alert.showMessage(payloadPlantillaEventoResponse);
+		if (operacionEnum.equals(OperacionEnum.MODIFICAR)) {
+			this.getEvento().getEventoIndicadores().clear();
+			this.getEvento().getEventoIndicadores()
+					.addAll(this.getEventoIndicadores());
+			this.getEvento().getEventoTareas().clear();
+			this.getEvento().getEventoTareas().addAll(this.getEventoTareas());
+			PayloadEventoResponse payloadEventoResponse = S.EventoService
+					.modificar(getEvento());
+			if (!UtilPayload.isOK(payloadEventoResponse)) {
+				Alert.showMessage(payloadEventoResponse);
 				return true;
 			}
+			Alert.showMessage(payloadEventoResponse);
 			DataCenter.reloadCurrentNodoMenu();
 			return true;
-			*/
-		}
 
-		if (operacionEnum.equals(OperacionEnum.MODIFICAR))
-		{
-			/*
-			PayloadPlantillaEventoResponse payloadPlantillaEventoResponse = S.PlantillaEventoService.modificar(getEvento());
-			if(!UtilPayload.isOK(payloadRolResponse))
-			{
-				Alert.showMessage(payloadPlantillaEventoResponse);
-				return true;
-			}
-			DataCenter.reloadCurrentNodoMenu();
-			return true;
-			*/
 		}
 		return false;
 	}
 
 	@Override
-	public boolean actionSalir(OperacionEnum operacionEnum)
-	{
+	public boolean actionSalir(OperacionEnum operacionEnum) {
 		DataCenter.reloadCurrentNodoMenu();
 		return true;
 	}
 
 	@Override
-	public boolean actionCancelar(OperacionEnum operacionEnum)
-	{
+	public boolean actionCancelar(OperacionEnum operacionEnum) {
 		return actionSalir(operacionEnum);
 	}
 
-	public Evento getEvento()
-	{
+	public Evento getEvento() {
 		return (Evento) DataCenter.getEntity();
 	}
 
-	public boolean isFormValidated()
-	{
-		try
-		{
+	public boolean isFormValidated() {
+		try {
 			return true;
-		}
-		catch (Exception e)
-		{
+		} catch (Exception e) {
 			Alert.showMessage(e.getMessage());
 			return false;
 		}
 	}
+
+	public List<ClasificadorEvento> getClasificadorEventos() {
+		if (this.clasificadorEventos == null) {
+			this.clasificadorEventos = new ArrayList<>();
+		}
+		if (this.clasificadorEventos.isEmpty()) {
+			PayloadClasificadorEventoResponse payloadClasificadorEventoResponse = S.ClasificadorEventoService
+					.consultarTodos();
+
+			if (!UtilPayload.isOK(payloadClasificadorEventoResponse)) {
+				Alert.showMessage(payloadClasificadorEventoResponse);
+			}
+
+			this.clasificadorEventos
+					.addAll(payloadClasificadorEventoResponse.getObjetos());
+		}
+		return clasificadorEventos;
+	}
+
+	public void setClasificadorEventos(
+			List<ClasificadorEvento> clasificadorEventos) {
+		this.clasificadorEventos = clasificadorEventos;
+	}
+
+	public List<Indicador> getIndicadores() {
+		if (this.indicadores == null) {
+			this.indicadores = new ArrayList<>();
+		}
+
+		return indicadores;
+	}
+
+	public void setIndicadores(List<Indicador> indicadores) {
+		this.indicadores = indicadores;
+	}
+
+	public Set<Indicador> getIndicadoresSeleccionados() {
+		if (this.indicadoresSeleccionados == null) {
+			this.indicadoresSeleccionados = new HashSet<>();
+		}
+		return indicadoresSeleccionados;
+	}
+
+	public void setIndicadoresSeleccionados(
+			Set<Indicador> indicadoresSeleccionados) {
+		this.indicadoresSeleccionados = indicadoresSeleccionados;
+	}
+
+	public List<Indicador> getEventoIndicadores() {
+		if (this.eventoIndicadores == null) {
+			eventoIndicadores = new ArrayList<>();
+		}
+		return eventoIndicadores;
+	}
+
+	public void setEventoIndicadores(List<Indicador> eventoIndicadores) {
+		this.eventoIndicadores = eventoIndicadores;
+	}
+
+	public Set<Indicador> getEventoIndicadoresSeleccionados() {
+		if (this.eventoIndicadoresSeleccionados == null) {
+			this.eventoIndicadoresSeleccionados = new HashSet<>();
+		}
+		return eventoIndicadoresSeleccionados;
+	}
+
+	public void setEventoIndicadoresSeleccionados(
+			Set<Indicador> eventoIndicadoresSeleccionados) {
+		this.eventoIndicadoresSeleccionados = eventoIndicadoresSeleccionados;
+	}
+
+	public List<Tarea> getTareas() {
+		if (this.tareas == null) {
+			this.tareas = new ArrayList<>();
+		}
+		return tareas;
+	}
+
+	public void setTareas(List<Tarea> tareas) {
+		this.tareas = tareas;
+	}
+
+	public Set<Tarea> getTareasSeleccionadas() {
+		if (this.tareasSeleccionadas == null) {
+			this.tareasSeleccionadas = new HashSet<>();
+		}
+		return tareasSeleccionadas;
+	}
+
+	public void setTareasSeleccionadas(Set<Tarea> tareasSeleccionadas) {
+		this.tareasSeleccionadas = tareasSeleccionadas;
+	}
+
+	public List<Tarea> getEventoTareas() {
+		if (this.eventoTareas == null) {
+			this.eventoTareas = new ArrayList<>();
+		}
+		return eventoTareas;
+	}
+
+	public void setEventoTareas(List<Tarea> eventoTareas) {
+		this.eventoTareas = eventoTareas;
+	}
+
+	public Set<Tarea> getEventoTareasSeleccionadas() {
+		if (this.eventoTareasSeleccionadas == null) {
+			this.eventoTareasSeleccionadas = new HashSet<>();
+		}
+		return eventoTareasSeleccionadas;
+	}
+
+	public void setEventoTareasSeleccionadas(
+			Set<Tarea> eventoTareasSeleccionadas) {
+		this.eventoTareasSeleccionadas = eventoTareasSeleccionadas;
+	}
+
 }
--- src/ve/smile/datos/configuracion/requisito_participacion/viewmodels/VM_RequisitoParticipacionIndex.java
+++ src/ve/smile/datos/configuracion/requisito_participacion/viewmodels/VM_RequisitoParticipacionIndex.java
@@ -4,38 +4,38 @@ import karen.core.crux.alert.Alert;
 import karen.core.simple_list_principal.viewmodels.VM_WindowSimpleListPrincipal;
 import karen.core.util.payload.UtilPayload;
 import lights.core.payload.response.IPayloadResponse;
-import ve.smile.consume.services.S;
-import ve.smile.dto.Participacion;
-import ve.smile.payload.response.PayloadParticipacionResponse;
-import ve.smile.payload.response.PayloadRequisitoParticipacionResponse;
-import ve.smile.seguridad.enums.OperacionEnum;
-
 
 import org.zkoss.bind.annotation.Command;
 import org.zkoss.bind.annotation.Init;
 
-public class VM_RequisitoParticipacionIndex extends VM_WindowSimpleListPrincipal<Participacion> {
+import ve.smile.consume.services.S;
+import ve.smile.dto.Participacion;
+import ve.smile.payload.response.PayloadParticipacionResponse;
+import ve.smile.payload.response.PayloadRequisitoResponse;
+import ve.smile.seguridad.enums.OperacionEnum;
+
+public class VM_RequisitoParticipacionIndex extends
+		VM_WindowSimpleListPrincipal<Participacion> {
 
 	@Init(superclass = true)
 	public void childInit() {
-		//NOTHING OK!
+		// NOTHING OK!
 	}
 
 	@Override
 	public IPayloadResponse<Participacion> getDataToTable(
 			Integer cantidadRegistrosPagina, Integer pagina) {
 
-		PayloadParticipacionResponse payloadParticipacionResponse = 
-				S.ParticipacionService.consultarPaginacion(cantidadRegistrosPagina, pagina);
+		PayloadParticipacionResponse payloadParticipacionResponse = S.ParticipacionService
+				.consultarPaginacion(cantidadRegistrosPagina, pagina);
 
 		return payloadParticipacionResponse;
 	}
 
-
 	@Override
 	public void doDelete() {
-		PayloadParticipacionResponse payloadParticipacionResponse =
-				S.ParticipacionService.eliminar(getSelectedObject().getIdParticipacion());
+		PayloadParticipacionResponse payloadParticipacionResponse = S.ParticipacionService
+				.eliminar(getSelectedObject().getIdParticipacion());
 
 		Alert.showMessage(payloadParticipacionResponse);
 
@@ -53,18 +53,20 @@ public class VM_RequisitoParticipacionIndex extends VM_WindowSimpleListPrincipal
 	@Command("onSelectParticipacion")
 	public void onSelectParticipacion() {
 		Participacion participacion = getSelectedObject();
-		
-		if (participacion.getRequisitoParticipacions() == null || participacion.getRequisitoParticipacions().size() == 0) {
-			
-			PayloadRequisitoParticipacionResponse payloadRequisitoParticipacionResponse = 
-					S.RequisitoParticipacionService.consultarPorParticipacion(participacion.getIdParticipacion());
-			
-			if (!(Boolean)payloadRequisitoParticipacionResponse.getInformacion(IPayloadResponse.IS_OK)) {
-				Alert.showMessage(payloadRequisitoParticipacionResponse);
+
+		if (participacion.getRequisitos() == null
+				|| participacion.getRequisitos().size() == 0) {
+
+			PayloadRequisitoResponse payloadRequisitoResponse = S.RequisitoService
+					.consultarPorParticipacion(participacion
+							.getIdParticipacion());
+
+			if (!UtilPayload.isOK(payloadRequisitoResponse)) {
+				Alert.showMessage(payloadRequisitoResponse);
 				return;
 			}
-			
-			participacion.setRequisitoParticipacions(payloadRequisitoParticipacionResponse.getObjetos());
-		}	
+
+			participacion.setRequisitos(payloadRequisitoResponse.getObjetos());
+		}
 	}
 }

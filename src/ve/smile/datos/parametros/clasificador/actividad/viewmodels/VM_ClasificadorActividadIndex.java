cb1e859f2d822ede200f350224e6d45a09b160e7
AHewstone <ahewstone@gmail.com>
2016-10-09 17:10:39 -0400

    Validacion de maestros para la eliminacion de un registro (Clasificadores, Tarea, Trabajo Social y Unidad de medida)

--- src/ve/smile/datos/parametros/clasificador/actividad/viewmodels/VM_ClasificadorActividadIndex.java
+++ src/ve/smile/datos/parametros/clasificador/actividad/viewmodels/VM_ClasificadorActividadIndex.java
@@ -1,14 +1,19 @@
 package ve.smile.datos.parametros.clasificador.actividad.viewmodels;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import karen.core.crux.alert.Alert;
 import karen.core.simple_list_principal.viewmodels.VM_WindowSimpleListPrincipal;
 import karen.core.util.payload.UtilPayload;
+import lights.core.enums.TypeQuery;
 import lights.core.payload.response.IPayloadResponse;
 
 import org.zkoss.bind.annotation.Init;
 
 import ve.smile.consume.services.S;
 import ve.smile.dto.ClasificadorActividad;
+import ve.smile.payload.response.PayloadActividadResponse;
 import ve.smile.payload.response.PayloadClasificadorActividadResponse;
 import ve.smile.seguridad.enums.OperacionEnum;
 
@@ -46,6 +51,36 @@ public class VM_ClasificadorActividadIndex extends
 	@Override
 	public String getSrcFileZulForm(OperacionEnum operacionEnum) {
 		return "views/desktop/datos/parametros/clasificador/actividad/ClasificadorActividadFormBasic.zul";
+	}
+	
+	@Override
+	public String isValidPreconditionsEliminar() {
+		if (getSelectedObject() == null) {
+			return "I:Information Code: 905-Debe seleccionar un Clasificador";
+		}
+
+		Map<String, String> criterios =
+				new HashMap<String, String>();
+
+		criterios.put("fkClasificadorActividad.idClasificadorActividad", String.valueOf(getSelectedObject().getIdClasificadorActividad()));
+
+		//Table Relation Actividad
+		PayloadActividadResponse payloadActividadResponse =
+				S.ActividadService.contarCriterios(TypeQuery.EQUAL,
+						criterios);
+
+		if (!UtilPayload.isOK(payloadActividadResponse)) {
+			return String.valueOf(payloadActividadResponse.getInformacion(IPayloadResponse.MENSAJE));
+		}
+
+		Integer countActividades = 
+				Double.valueOf(String.valueOf(payloadActividadResponse.getInformacion(IPayloadResponse.COUNT))).intValue();
+	
+		if (countActividades > 0) {
+			return "E:Error 0:No se puede eliminar el <b>Clasificador</b> seleccionado ya que está siendo utilizado en " + countActividades + " Registros de Actividades";
+		}
+
+		return "";
 	}
 
 }
--- src/ve/smile/datos/parametros/unidad_medida/viewmodels/VM_UnidadMedidaIndex.java
+++ src/ve/smile/datos/parametros/unidad_medida/viewmodels/VM_UnidadMedidaIndex.java
@@ -1,19 +1,25 @@
 package ve.smile.datos.parametros.unidad_medida.viewmodels;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import karen.core.crux.alert.Alert;
 import karen.core.simple_list_principal.viewmodels.VM_WindowSimpleListPrincipal;
 import karen.core.util.payload.UtilPayload;
+import lights.core.enums.TypeQuery;
 import lights.core.payload.response.IPayloadResponse;
 
 import org.zkoss.bind.annotation.Init;
 
 import ve.smile.consume.services.S;
 import ve.smile.dto.UnidadMedida;
+import ve.smile.payload.response.PayloadIndicadorResponse;
+import ve.smile.payload.response.PayloadRecursoResponse;
 import ve.smile.payload.response.PayloadUnidadMedidaResponse;
 import ve.smile.seguridad.enums.OperacionEnum;
 
 public class VM_UnidadMedidaIndex extends
-		VM_WindowSimpleListPrincipal<UnidadMedida> {
+VM_WindowSimpleListPrincipal<UnidadMedida> {
 
 	@Init(superclass = true)
 	public void childInit() {
@@ -46,6 +52,52 @@ public class VM_UnidadMedidaIndex extends
 	@Override
 	public String getSrcFileZulForm(OperacionEnum operacionEnum) {
 		return "views/desktop/datos/parametros/unidadMedida/UnidadMedidaFormBasic.zul";
+	}
+
+	@Override
+	public String isValidPreconditionsEliminar() {
+		if (getSelectedObject() == null) {
+			return "I:Information Code: 905-Debe seleccionar una Unidad de Medida";
+		}
+
+		Map<String, String> criterios =
+				new HashMap<String, String>();
+
+		criterios.put("fkUnidadMedida.idUnidadMedida", String.valueOf(getSelectedObject().getIdUnidadMedida()));
+
+		//Table Relation Indicador
+		PayloadIndicadorResponse payloadIndicadorResponse =
+				S.IndicadorService.contarCriterios(TypeQuery.EQUAL,
+						criterios);
+
+		if (!UtilPayload.isOK(payloadIndicadorResponse)) {
+			return String.valueOf(payloadIndicadorResponse.getInformacion(IPayloadResponse.MENSAJE));
+		}
+
+		Integer countIndicadores = 
+				Double.valueOf(String.valueOf(payloadIndicadorResponse.getInformacion(IPayloadResponse.COUNT))).intValue();
+
+		//Table Relation Recurso
+		PayloadRecursoResponse payloadRecursoResponse =
+				S.RecursoService.contarCriterios(TypeQuery.EQUAL,
+						criterios);
+
+		if (!UtilPayload.isOK(payloadRecursoResponse)) {
+			return String.valueOf(payloadRecursoResponse.getInformacion(IPayloadResponse.MENSAJE));
+		}
+
+		Integer countRecursos = 
+				Double.valueOf(String.valueOf(payloadRecursoResponse.getInformacion(IPayloadResponse.COUNT))).intValue();
+
+		if (countIndicadores > 0) {
+			return "E:Error 0:No se puede eliminar la <b>Unidad de Medida</b> seleccionada ya que está siendo utilizada en " + countIndicadores + " Indicadores";
+		}
+
+		if (countRecursos > 0) {
+			return "E:Error 0:No se puede eliminar la <b>Unidad de Medida</b> seleccionada ya que está siendo utilizada en " + countRecursos + " Recursos";
+		}
+
+		return "";
 	}
 
 }

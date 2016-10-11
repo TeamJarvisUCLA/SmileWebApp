64b1b3550ec7cce6ec03304131cd57562d55ce3c
luisdpp <luisdpp210491@gmail.com>
2016-10-10 22:08:13 -0400

    Pantalla de administracion de comentarios de  los albumes

---
+++ src/ve/smile/administracion/portalweb/comentarios/album/viewmodels/VM_ComentariosAlbumFormBasic.java
@@ -0,0 +1,97 @@
+package ve.smile.administracion.portalweb.comentarios.album.viewmodels;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.zkoss.bind.annotation.Init;
+
+import ve.smile.consume.services.S;
+import ve.smile.dto.ComentarioAlbum;
+import ve.smile.enums.EstatusComentarioAlbumEnum;
+import ve.smile.payload.response.PayloadComentarioAlbumResponse;
+import ve.smile.seguridad.enums.OperacionEnum;
+import karen.core.crux.alert.Alert;
+import karen.core.crux.session.DataCenter;
+import karen.core.form.buttons.data.OperacionForm;
+import karen.core.form.buttons.enums.OperacionFormEnum;
+import karen.core.form.buttons.helpers.OperacionFormHelper;
+import karen.core.form.viewmodels.VM_WindowForm;
+import karen.core.util.payload.UtilPayload;
+import karen.core.util.validate.UtilValidate;
+
+
+public class VM_ComentariosAlbumFormBasic extends VM_WindowForm{
+
+	@Init(superclass = true)
+	public void childInit() {
+		
+	}
+
+	@Override
+	public List<OperacionForm> getOperationsForm(OperacionEnum operacionEnum) {
+		List<OperacionForm> operacionesForm = new ArrayList<OperacionForm>();
+		if(operacionEnum.equals(OperacionEnum.CUSTOM1)) {
+			operacionesForm.add(OperacionFormHelper.getPorType(OperacionFormEnum.APROBAR));
+			operacionesForm.add(OperacionFormHelper.getPorType(OperacionFormEnum.RECHAZAR));
+			return operacionesForm;
+		}
+		return operacionesForm;
+	}
+
+	
+
+	@Override
+	public boolean actionAprobar(OperacionEnum operacionEnum) {
+		getComentarioAlbum().setEstatusComentario(EstatusComentarioAlbumEnum.PUBLICADO.ordinal());
+		PayloadComentarioAlbumResponse PayloadComentarioAlbumResponse =S.ComentarioAlbumService.modificar(getComentarioAlbum());
+		if (!UtilPayload.isOK(PayloadComentarioAlbumResponse)) {
+			Alert.showMessage(PayloadComentarioAlbumResponse);
+			return true;
+		}
+		DataCenter.reloadCurrentNodoMenu();
+		return false;
+	}
+
+	@Override
+	public boolean actionRechazar(OperacionEnum operacionEnum) {
+		getComentarioAlbum().setEstatusComentario(EstatusComentarioAlbumEnum.OCULTO.ordinal());
+		PayloadComentarioAlbumResponse PayloadComentarioAlbumResponse =S.ComentarioAlbumService.modificar(getComentarioAlbum());
+		if (!UtilPayload.isOK(PayloadComentarioAlbumResponse)) {
+			Alert.showMessage(PayloadComentarioAlbumResponse);
+			return true;
+		}
+		DataCenter.reloadCurrentNodoMenu();
+		return false;
+	}
+
+	public ComentarioAlbum getComentarioAlbum() {
+		return (ComentarioAlbum) DataCenter.getEntity();
+	}
+
+	public boolean isFormValidated() {
+		try {
+			UtilValidate.validateString(getComentarioAlbum().getFkComunidad().getNombre(), "Nombre", 200);
+			UtilValidate.validateString(getComentarioAlbum().getFkComunidad().getCorreo(), "Correo", 200);
+			UtilValidate.validateString(getComentarioAlbum().getComentario(), "Comentario", 300);
+			//UtilValidate.validateDate(getComentarioAlbum().getFecha(), "Fecha", validateOperator, date_8601, formatToShow);
+			//UtilValidate.validateInteger(getComentarioAlbum().getTipoContactoPortal(), "Tipo Contacto", validateOperator, valueToCompare);
+			UtilValidate.validateNull(getComentarioAlbum().getPuntuacion(), "Puntuacion");
+			
+			return true;
+		} catch (Exception e) {
+			Alert.showMessage(e.getMessage());
+			return false;
+		}
+	}
+
+	@Override
+	public boolean actionCancelar(OperacionEnum operacionEnum) {
+		return actionSalir(operacionEnum);
+	}
+
+	@Override
+	public boolean actionSalir(OperacionEnum operacionEnum) {
+		DataCenter.reloadCurrentNodoMenu();
+		return true;
+	}
+}
